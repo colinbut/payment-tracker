9e50fffe764429e866dbd7add4bcc78c63cda264
Colin But <colinbut@users.noreply.github.com>
2019-03-09 15:56:37 +0000

    PaymentTracker implementation

--- kotlin/src/main/kotlin/payment/tracker/PaymentTracker.kt
+++ kotlin/src/main/kotlin/payment/tracker/PaymentTracker.kt
@@ -1,9 +1,38 @@
 package payment.tracker
 
-class PaymentTracker {
-
-}
+import payment.tracker.io.PaymentTrackerReader
+import payment.tracker.util.UsagePrinter
+import java.lang.IllegalArgumentException
+import java.util.*
 
 fun main(args: Array<String>) {
-    println(PaymentTracker())
+
+    val parser = Parser()
+
+    PaymentTrackerReader.readInputFile("src/main/resources/initial_input_file.txt")
+
+    if (args.size > 1) {
+        UsagePrinter.printUsage()
+        throw IllegalArgumentException("Invalid number of arguments")
+    }
+
+    if (args.size == 1){
+        val optionalInputFile = args[0]
+        val input = PaymentTrackerReader.readInputFile(optionalInputFile)
+    }
+
+    val scanner = Scanner(System.`in`)
+
+    while(true) {
+        print("Enter something: ")
+        val line : String = scanner.nextLine()
+        if (line.equals("quit", true) || line.equals("q", true)) {
+            print("Exiting!")
+            break;
+        }
+        parser.invoke(line)
+    }
+
+    scanner.close()
+
 }
